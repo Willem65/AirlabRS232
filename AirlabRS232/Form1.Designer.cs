49a290b0d2e844f1200a8e15f08f897159104633
Willem65 <Willempost77@gmail.com>
2022-09-23 21:46:43 +0200

    oke
    oke

--- AirlabRS232/Form1.Designer.cs
+++ AirlabRS232/Form1.Designer.cs
@@ -893,8 +893,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1355, 932);
+            this.ClientSize = new System.Drawing.Size(1355, 749);
             this.Controls.Add(this.label18);
             this.Controls.Add(this.label17);
             this.Controls.Add(this.label16);
